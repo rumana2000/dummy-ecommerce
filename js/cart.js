7e0fac5f8442b79175dc4a437e02d8bff4c427d1
Sayeda Rumana <sayeda.rumana07@gmail.com>
2023-05-07 23:34:29 +0600

    feat : Basic operation done

---
+++ js/cart.js
@@ -0,0 +1,136 @@
+const API_BASE_URL = "https://dummyjson.com"
+const DATA_STORAGE = "allItemData"
+
+let allItemData = JSON.parse(localStorage.getItem(DATA_STORAGE)) ?? {}
+let cartItem = document.querySelector("#showCartItem")
+let counter = document.querySelector("#show-cart-item-counter")
+let oderSummary = document.querySelector("#show-oder-summary")
+let cartProducts = [];
+
+function renderCartUI() {
+    let allCartItem = JSON.parse(localStorage.getItem(DATA_STORAGE)) ?? {};
+    let cartItemHtml = ""
+    cartProducts = [];
+
+    fetch(`${API_BASE_URL}/products?limit=100`)
+        .then((response) => response.json())
+        .then((data) => {
+            let alldataProduct = data.products
+            for (let i = 0; i < alldataProduct.length; i++) {
+                let productId = alldataProduct[i].id
+
+                if (allCartItem[productId] != undefined) {
+                    cartProducts.push(alldataProduct[i])
+                }
+            }
+            for (let i = 0; i < cartProducts.length; i++) {
+
+                let regularPrice = cartProducts[i].price;
+                let discountPrice = cartProducts[i].discountPercentage;
+                let discountAblePrice = (regularPrice * (discountPrice / 100)).toPrecision(2);
+                let totalPrice = regularPrice - discountAblePrice;
+
+                cartItemHtml += `<div class="single-product-item">
+               <div class="card mb-3">
+                   <div class="row g-0">
+                   <div class="col-md-2 d-flex align-items-center">
+                       <div class="cart-item-image">
+                           <img src="${cartProducts[i].thumbnail}" class="img-fluid" alt="...">
+                       </div>
+                   </div>
+                   <div class="col-md-7">
+                       <div class="card-body">
+                           <h5 class="card-title">${cartProducts[i].title}</h5>
+                           <p class="card-text">
+                               <small class="text-body-secondary">${cartProducts[i].brand}</small>
+                           </p>
+                           <div class="">
+                               <span class="discount-price">$${totalPrice}</span>
+                               <span class="regular-price">$${cartProducts[i].price}</span>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="col-md-3">
+                       <div class="cart-action">
+                           <div class="cart-item-quantity">
+                               <button class="btn btn-outline-warning" data-id="${cartProducts[i].id}" onclick="dicriment(this)" ><i class="bi bi-dash"></i></button>
+                               <input type="text" class="form-control cart-item-quantity-input" value="${allItemData[cartProducts[i].id]['quantity']}">
+                               <button class="btn btn-outline-warning"data-id="${cartProducts[i].id}" onclick="incrimant(this)"  ><i class="bi bi-plus"></i></button>
+                           </div>
+                           <div class="cart-item-delete">
+                               <button class="btn btn-danger" data-id="${cartProducts[i].id}" onclick="removeitem(this)"><i class="bi bi-trash3-fill"></i></button>
+                           </div>
+                       </div>
+                       
+                   </div>
+                   </div>
+               </div>
+           </div>`
+            }
+
+            cartItem.innerHTML = cartItemHtml
+            ShowsummaryUI()
+        })
+
+}
+renderCartUI()
+
+function ShowsummaryUI() {
+    let allItemData = JSON.parse(localStorage.getItem(DATA_STORAGE)) ?? {};
+    let odrderSummaryHtml = ""
+    let sum = 0 
+    for (let i = 0; i < cartProducts.length; i++) {
+        let subTotal = cartProducts[i].price * allItemData[cartProducts[i].id]['quantity']
+        sum = sum + subTotal
+        odrderSummaryHtml += `<div class="order-summry-info">
+            <p>${cartProducts[i].title} X ${allItemData[cartProducts[i].id]['quantity']}</p>
+            <p>$${subTotal}</p>
+        </div>`
+    }
+    oderSummary.innerHTML = odrderSummaryHtml
+    document.querySelector("#show-price").innerHTML = sum
+
+}
+
+function incrimant(e) {
+    let id = e.getAttribute("data-id")
+    let setQuantity = e.parentNode
+    console.log(setQuantity);
+    let quantity = setQuantity.querySelector(".cart-item-quantity-input").value
+    setQuantity.querySelector(".cart-item-quantity-input").value = (parseInt(quantity)+1)
+    let allItemData = JSON.parse(localStorage.getItem(DATA_STORAGE)) ?? {};
+    allItemData[id]['quantity'] = allItemData[id]['quantity'] + 1
+    localStorage.setItem(DATA_STORAGE, JSON.stringify(allItemData)) ?? {};
+    ShowsummaryUI()
+
+}
+function dicriment(e) {
+    let id = e.getAttribute("data-id")
+    let deleteQuantity = e.parentNode
+    let quantity = deleteQuantity.querySelector(".cart-item-quantity-input").value
+    if(quantity > 1) {
+    deleteQuantity.querySelector(".cart-item-quantity-input").value = (parseInt(quantity) - 1)
+    let allItemData = JSON.parse(localStorage.getItem(DATA_STORAGE)) ?? {};
+    allItemData[id]['quantity'] = allItemData[id]['quantity'] - 1
+    localStorage.setItem(DATA_STORAGE,JSON.stringify(allItemData)) ?? {};
+    }
+    ShowsummaryUI()
+}
+
+function removeitem(e) {
+    let ID = e.getAttribute("data-id")
+    let allItemData = JSON.parse(localStorage.getItem(DATA_STORAGE)) ?? {};
+    delete allItemData[ID]
+    localStorage.setItem(DATA_STORAGE, JSON.stringify(allItemData)) ?? {};
+    renderCartUI()
+    countCart()
+}
+
+function countCart() {
+    let allItemData = JSON.parse(localStorage.getItem(DATA_STORAGE)) ?? {};
+    let length = Object.keys(allItemData).length
+    counter.innerHTML = (length);
+}
+
+
+countCart()
---
+++ js/script.js
@@ -0,0 +1,168 @@
+const API_BASE_URL = "https://dummyjson.com"
+const DATA_STORAGE = "allItemData"
+
+let allCetagory = document.querySelector("#showCategories")
+let allProduct = document.querySelector("#showAllItem")
+let pagination = document.querySelector("#showPagination")
+let counterMsg = document.querySelector("#show-cart-item-counter")
+let counter = document.querySelector("#show-cart-item-counter")
+let allItemData = JSON.parse(localStorage.getItem(DATA_STORAGE)) ?? {};
+let cartProducts = [];
+
+let PER_PAGE = 12
+
+let currentPage = 1
+let totalProduct = 0
+
+
+
+
+function search() {
+    let searchInput = document.getElementById('input-item').value
+    url = "https://dummyjson.com/products"
+    if (searchInput.length > 0) {
+        url = `https://dummyjson.com/products/search?q=${searchInput}`
+    } else {
+        url = "https://dummyjson.com/products"
+    }
+    showAllProduct(url)
+}
+
+function showAllProduct(url) {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            totalProduct = data.total;
+            let alldataProduct = data.products;
+
+            let allProductHTML = ""
+            // use foreach loop here
+            for (let i = 0; i < alldataProduct.length; i++) {
+                let regularPrice = alldataProduct[i].price;
+                let discountPrice = alldataProduct[i].discountPercentage;
+                let discountAblePrice = (regularPrice * (discountPrice / 100)).toPrecision(2);
+                let totalPrice = regularPrice - discountAblePrice;
+
+                let stockInfo = []
+                if (alldataProduct[i].stock > 20) {
+                    stockInfo.msg = "In-Stock";
+                    stockInfo.stockTxtColor = "text-success";
+                } else if (alldataProduct[i].stock > 0 && alldataProduct[i].stock > 1) {
+                    stockInfo.msg = "Low-Stock";
+                    stockInfo.stockTxtColor = "text-warning";
+                } else if (alldataProduct[i].stock <= 0) {
+                    stockInfo.msg = "Out-Of-Stock"
+                    stockInfo.stockTxtColor = "text-danger"
+                }
+                let ratingValue = alldataProduct[i].rating;
+                let ratingDom = ""
+                for (let i = 0; i < 5; i++) {
+                    if (ratingValue >= 1) {
+                        ratingDom += `<li><a> <i class="bi bi-star-fill checked"></i> </a></li>`
+                    } else if (ratingValue > 0 && ratingValue < 1) {
+                        ratingDom += `<li><a> <i class="bi bi-star-half checked"></i> </a></li>`
+                    } else if (ratingValue >= 0) {
+                        ratingDom += `<li><a> <i class="bi bi-star"></i> </a></li>`
+                    }
+                    ratingValue = ratingValue - 1
+                }
+
+
+                allProductHTML += `<div class="col-md-3 col-sm-6 mt-3">
+            <div class="product-image">
+                <div class="image">
+                    <img src="${alldataProduct[i].thumbnail}" alt="">
+                </div>
+                <p class="product-brand text-center">${alldataProduct[i].brand}</p>
+                <p class="product-name">${alldataProduct[i].title}</p>
+                <div class="price-section">
+                    <span class="discount-price">$${totalPrice}</span>
+                    <span class="regular-price">$${alldataProduct[i].price}</span>
+                </div>
+                <p class="${stockInfo.stockTxtColor} text-center">${stockInfo.msg} <i class="bi bi-check-circle"></i></p>
+                <div class="reting-section">
+                ${alldataProduct[i].rating}
+                    <ul class="star">
+                    ${ratingDom}
+                    </ul>
+                </div>
+                <div class="button-section">
+                    <button type="button" class="cart-button" data-id="${alldataProduct[i].id}" onclick="addnewItem(this)"><i class="bi bi-cart-check"></i></button>
+                </div>
+            </div>
+        </div>`
+            }
+            allProduct.innerHTML = allProductHTML;
+            showPagination()
+        })
+}
+showAllProduct(`${API_BASE_URL}/products?skip=0&limit=${PER_PAGE}`);
+
+
+
+function showPagination() {
+    let numberOfPagination = Math.ceil(totalProduct / PER_PAGE)
+    let paginationHtml = ""
+    for (let i = 1; i <= numberOfPagination; i++) {
+        if (currentPage == i) {
+            paginationHtml += `<li class="page-item disabled"><button  data-index="${i}" onclick="loadPaginationProduct(this)"class="page-link" >${i}</button></li>`
+        } else {
+            paginationHtml += `<li class="page-item"><button  data-index="${i}" onclick="loadPaginationProduct(this)" class="page-link">${i}</button></li>`
+        }
+    }
+    pagination.innerHTML = paginationHtml;
+}
+
+function loadPaginationProduct(e) {
+    let paginationIndex = e.getAttribute("data-index")
+    currentPage = paginationIndex;
+
+    let skip = PER_PAGE * (paginationIndex - 1)
+
+    let url = `${API_BASE_URL}/products?skip=${skip}&limit=${PER_PAGE}`
+
+    showAllProduct(url)
+}
+function showAllCatagories() {
+    fetch(`${API_BASE_URL}/products/categories`)
+        .then((response) => response.json())
+        .then((data) => {
+            let allCetagoryHTML = ""
+            for (let index = 0; index < data.length; index++) {
+                allCetagoryHTML += `<button type="button" class="btn btn-outline-warning m-1"  data-id="${data[index]}" onclick="loadCetagoryProduct(this)">${data[index]} </button>`
+
+            }
+            allCetagory.innerHTML = allCetagoryHTML
+        })
+}
+
+showAllCatagories()
+
+function loadCetagoryProduct(e) {
+    let slug = e.getAttribute("data-id")
+    console.log(slug);
+    url = `${API_BASE_URL}/products/category/${slug}`
+    showAllProduct(url)
+
+}
+
+function addnewItem(e) {
+    let ID = e.getAttribute("data-id")
+    let newItem = {
+        newID : ID,
+        quantity : 1
+    }
+    allItemData[ID] = newItem
+    localStorage.setItem(DATA_STORAGE,JSON.stringify(allItemData))
+    countCart ()
+}
+
+function countCart () {
+ let allItemData = JSON.parse(localStorage.getItem(DATA_STORAGE))
+ if (allItemData) {
+    let length = Object.keys(allItemData).length
+    counter.innerHTML = (length);
+ }
+
+}
+countCart()
